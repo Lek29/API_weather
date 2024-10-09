d3fe98690dc494465016c5691e21a6b818ae815d
Lek29 <ligioner29@mail.ru>
2024-10-09 11:00:11 +0800

    правильно оформил словарь v2

--- weather.py
+++ weather.py
@@ -2,7 +2,6 @@ import requests
 
 
 def main():
-    url_sunfrancisco = 'https://wttr.in/sun%20francisco'
     variable_london = 'Лондон'
     variable_moscov_airport_sheremetivo = 'SVO'
     variable_cherepovec = 'Череповец'
@@ -10,15 +9,18 @@ def main():
         'lang': 'ru',
         'n': '',
         'q': '',
-        'T':'',
-        'M':'',
+        'T': '',
+        'M': '',
     }
+
     weather_cherepovec = f'https://wttr.in/{variable_cherepovec}'
     weather_sheremetivo = f'https://wttr.in/{variable_moscov_airport_sheremetivo}'
     weather_london = f'https://wttr.in/{variable_london}'
+
     response_cherepovec = requests.get(weather_cherepovec, params=charcteristic_windows)
     response_sheremetivo = requests.get(weather_sheremetivo, params=charcteristic_windows)
     response_london = requests.get(weather_london, params=charcteristic_windows)
+
     response_cherepovec.raise_for_status()
     print(response_cherepovec.text)
     response_sheremetivo.raise_for_status()
