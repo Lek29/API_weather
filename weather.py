4fc7cd753911e000cdac007e4ee41d8a179c90d1
Lek29 <ligioner29@mail.ru>
2024-10-09 10:56:44 +0800

    изменил передачу аргументов в get запрос

--- weather.py
+++ weather.py
@@ -6,12 +6,26 @@ def main():
     variable_london = 'Лондон'
     variable_moscov_airport_sheremetivo = 'SVO'
     variable_cherepovec = 'Череповец'
-    url_template = f'https://wttr.in/{variable_cherepovec}?lang=ru&?n&?q&?T&?M'
-    response = requests.get(url_template)
-    response.raise_for_status()
-
-    return response.text
+    charcteristic_windows = {
+        'lang': 'ru',
+        'n': '',
+        'q': '',
+        'T':'',
+        'M':''
+    }
+    weather_cherepovec = f'https://wttr.in/{variable_cherepovec}'
+    weather_sheremetivo = f'https://wttr.in/{variable_moscov_airport_sheremetivo}'
+    weather_london = f'https://wttr.in/{variable_london}'
+    response_cherepovec = requests.get(weather_cherepovec, params=charcteristic_windows)
+    response_sheremetivo = requests.get(weather_sheremetivo, params=charcteristic_windows)
+    response_london = requests.get(weather_london, params=charcteristic_windows)
+    response_cherepovec.raise_for_status()
+    print(response_cherepovec.text)
+    response_sheremetivo.raise_for_status()
+    print(response_sheremetivo.text)
+    response_london.raise_for_status()
+    print(response_london.text)
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
